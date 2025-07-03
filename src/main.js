dda5029350ec1c7701075c6f6118b179713da823
Alexander <acruz@fi.uba.ar>
2025-07-03 18:03:36 -0300

    Modificado

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.1/build/three.module.js';
-
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.1/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -9,34 +9,25 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-camera.position.set(5,5,5);
+// Orbit Controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
 
-camera.lookAt(0,0,0);
-
-
-
-
-//-------------------------------------------------------------
-
+// Cubo
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
-
-const axesHelper = new THREE.axesHelper(5);
-scene.add(axesHelper);
-
-
-
-
-
+// Cámara
+camera.position.set(2, 2, 5);
+controls.update();  // necesario para aplicar damping antes del primer render
 
 function animate() {
-
+  cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
+  controls.update();  // importante si enableDamping está activado
   renderer.render( scene, camera );
-
 }
